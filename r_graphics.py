0befd5ba87a6b3c769d8b20f150d6a378504a817
Franaz96 <fgfrancescogariboldi1@gmail.com>
2025-02-28 13:32:10 +0100

    fixed best_models extraction

--- r_graphics.py
+++ r_graphics.py
@@ -92,7 +92,7 @@ def plot_best_models_diagnostics_ggplot2(best_models, df_r):
     # Plot for non-mixed model if available
     if best_models.get('non_mixed_best_model'):
         # Retrieve the formula for the non-mixed model (Python string)
-        non_mixed_best_formula = best_models['non_mixed_best_model']['formula']
+        non_mixed_best_formula = best_models['non_mixed_best_model'][0]['formula']
         # Convert the formula to an R string and assign it to the R
         # environment
         r_non_mixed_best_formula = rpy2.robjects.StrVector([non_mixed_best_formula])
@@ -150,7 +150,7 @@ def plot_best_models_diagnostics_ggplot2(best_models, df_r):
 
     # Plot for mixed model if available
     if best_models.get('mixed_best_model'):
-        mixed_best_formula = best_models['mixed_best_model']['formula']
+        mixed_best_formula = best_models['mixed_best_model'][0]['formula']
         r_mixed_best_formula = rpy2.robjects.StrVector([mixed_best_formula])
         rpy2.robjects.globalenv['mixed_best_formula'] = r_mixed_best_formula[0]
         print(f"Mixed model formula: {mixed_best_formula}")
